76680d9b15b42d7ae018b53ebde5212d01057f7b
Rachel Rosen <rachelrosen@google.com>
2017-11-15 18:11:44 -0500

    Trying to debug the test failing issue

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
+      { pattern: './node_modules/twemoji/2/twemoji.js' },
       { pattern: './src/test.ts', watched: false }
     ],
     preprocessors: {
--- src/app/convai-checker.component.spec.ts
+++ src/app/convai-checker.component.spec.ts
@@ -39,6 +39,7 @@ import { ConvaiChecker, DEFAULT_DEMO_SETTINGS, DemoSettings } from './convai-che
 import { PerspectiveApiService } from './perspectiveapi.service';
 import { AnalyzeCommentResponse } from './perspectiveapi-types';
 import 'gsap';
+import 'twemoji';
 
 @Component({
   selector: 'test-comp',
