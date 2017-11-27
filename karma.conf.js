2919700b67fdd989e8714aa0165fb94f240329bd
Rachel Rosen <rachelrosen@google.com>
2017-11-27 11:34:57 -0500

    Switches the test browser back to Chrome now that I'm working from my workstation and not remotely.

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false,
     /**
      * Maximum number of tries a browser will attempt in the case of a
