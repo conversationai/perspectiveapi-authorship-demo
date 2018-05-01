cb2c4107db1e15006cf98ca8ad177d8525ea5628
Rachel Rosen <rachelrosen@google.com>
2018-05-01 13:23:42 -0400

    Adds comment on a TODO for angular elements.

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,5 @@
 export * from './src/app/modules/convai-checker/convai-checker.module'
+
+// TODO(rachelrosen): Look into whether exporting from the angular elements
+// module for convai-checker will produce packaged code that can be used with
+// the angular elements webcomponent for convai-checker.
