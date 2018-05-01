e51264bd54c65a2b9d0f4f981a3dfd21368cb2e5
Rachel Rosen <rachelrosen@google.com>
2018-05-01 17:00:27 -0400

    Updating package.json version and exports.

--- public_api.ts
+++ public_api.ts
@@ -1,4 +1,6 @@
 export * from './src/app/modules/convai-checker/convai-checker.module'
+export * from './src/app/modules/convai-checker/convai-checker.component'
+export * from './src/app/modules/convai-checker/perspective-status.component'
 
 // TODO(rachelrosen): Look into whether exporting from the angular elements
 // module for convai-checker will produce packaged code that can be used with
