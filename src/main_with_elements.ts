ae460b8cab660108de0a2a59ade6bbb82f498fbf
Rachel Rosen <rachelrosen@google.com>
2018-07-13 18:04:20 -0400

    Updates angular elements to the Angular 6 version, and adds a separate build path for building the demo using elements.

--- src/main_with_elements.ts
+++ src/main_with_elements.ts
@@ -1,15 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {ElementDemoModule} from './app/modules/convai-checker/element-demo.module';
+import { ElementDemoModule } from './app/modules/convai-checker/element-demo.module';
 import { environment } from './environments/environment';
-import { registerAsCustomElements } from '@angular/elements';
-import {ConvaiChecker} from './app/modules/convai-checker/convai-checker.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-registerAsCustomElements([ConvaiChecker], () =>
-  platformBrowserDynamic().bootstrapModule(ElementDemoModule),
-);
+platformBrowserDynamic().bootstrapModule(ElementDemoModule);
