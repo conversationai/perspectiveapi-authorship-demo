55f0a26a2bcd5d18b418d766b66df94143488942
Daniel Borkan <dborkan@google.com>
2018-07-17 12:07:43 -0400

    Removing dist files

--- dist/conversationai-perspectiveapi-authorship-demo.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { PerspectiveApiService as ɵa } from './src/app/modules/convai-checker/perspectiveapi.service';
--- dist/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/app/modules/convai-checker/convai-checker.module';
-export * from './src/app/modules/convai-checker/convai-checker.component';
-export * from './src/app/modules/convai-checker/perspective-status.component';
