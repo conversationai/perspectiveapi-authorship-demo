8e4a46bd5023f3188ca08b0e53481d633cbaa526
Lucas Dos Santos <lucasdossantos@google.com>
2019-10-18 12:02:23 -0400

    Reformat perspectiveapi.service.ts

--- src/app/modules/convai-checker/perspectiveapi.service.ts
+++ src/app/modules/convai-checker/perspectiveapi.service.ts
@@ -13,31 +13,20 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {from, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
-import {
-  AnalyzeCommentData,
-  AnalyzeCommentRequest,
-  AnalyzeCommentResponse,
-  AttributeScores,
-  PerspectiveGapiClient,
-  RequestedAttributes,
-  SuggestCommentScoreData,
-  SuggestCommentScoreRequest,
-  SuggestCommentScoreResponse,
-} from './perspectiveapi-types';
+import {AnalyzeCommentData, AnalyzeCommentRequest, AnalyzeCommentResponse, AttributeScores, PerspectiveGapiClient, RequestedAttributes, SuggestCommentScoreData, SuggestCommentScoreRequest, SuggestCommentScoreResponse,} from './perspectiveapi-types';
 
 // TODO: Make this configurable for dev vs prod.
-const DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery'
-    + '/rest?version=v1alpha1';
+const DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery' +
+    '/rest?version=v1alpha1';
 const TOXICITY_ATTRIBUTE = 'TOXICITY';
 
 @Injectable()
 export class PerspectiveApiService {
-
   private gapiClient: PerspectiveGapiClient = null;
 
   constructor(private httpClient: HttpClient) {}
@@ -48,26 +37,32 @@ export class PerspectiveApiService {
     }
     gapi.load('client', () => {
       console.log('Starting to load gapi client');
-      (gapi.client as any).init({
-        'apiKey': apiKey,
-        'discoveryDocs': [ DISCOVERY_URL ],
-      }).then(() => {
-        console.log('Finished loading gapi client');
-        console.log(gapi.client);
-        this.gapiClient = (gapi.client as any) as PerspectiveGapiClient;
-      }, (error: Error) => {
-        console.error('Error loading gapi client:', error);
-      });
+      (gapi.client as any)
+          .init({
+            'apiKey': apiKey,
+            'discoveryDocs': [DISCOVERY_URL],
+          })
+          .then(
+              () => {
+                console.log('Finished loading gapi client');
+                console.log(gapi.client);
+                this.gapiClient = (gapi.client as any) as PerspectiveGapiClient;
+              },
+              (error: Error) => {
+                console.error('Error loading gapi client:', error);
+              });
     });
   }
 
   // TODO: this should be a Single observable, not a general observable because
   // any call to checkText will only give a single result.
-  checkText(data: AnalyzeCommentData, makeDirectApiCall: boolean,
-            serverUrl?: string): Observable<AnalyzeCommentResponse> {
+  checkText(
+      data: AnalyzeCommentData, makeDirectApiCall: boolean,
+      serverUrl?: string): Observable<AnalyzeCommentResponse> {
     if (makeDirectApiCall && this.gapiClient === null) {
-      console.error('No gapi client found; call initGapiClient with your API'
-                    + 'key to make a direct API call. Using server instead');
+      console.error(
+          'No gapi client found; call initGapiClient with your API' +
+          'key to make a direct API call. Using server instead');
       makeDirectApiCall = false;
     }
     if (makeDirectApiCall) {
@@ -83,37 +78,38 @@ export class PerspectiveApiService {
         session_id: data.sessionId,
         community_id: data.communityId
       };
-      return from(
-         this.gapiClient.commentanalyzer.comments.analyze(request))
-         .pipe(map(response => response.result));
+      return from(this.gapiClient.commentanalyzer.comments.analyze(request))
+          .pipe(map(response => response.result));
     } else {
       if (serverUrl === undefined) {
         serverUrl = '';
-        console.error('No server url specified for a non-direct API call.'
-                      + ' Defaulting to current hosted address');
+        console.error(
+            'No server url specified for a non-direct API call.' +
+            ' Defaulting to current hosted address');
       }
 
       const headers = new HttpHeaders();
       headers.append('Content-Type', 'application/json');
 
-      return this.httpClient.post(
-        serverUrl + '/check', data, {headers});
+      return this.httpClient.post(serverUrl + '/check', data, {headers});
     }
   }
 
-  suggestScore(data: SuggestCommentScoreData, makeDirectApiCall: boolean,
-               serverUrl?: string): Observable<SuggestCommentScoreResponse> {
+  suggestScore(
+      data: SuggestCommentScoreData, makeDirectApiCall: boolean,
+      serverUrl?: string): Observable<SuggestCommentScoreResponse> {
     if (makeDirectApiCall && this.gapiClient === null) {
-      console.error('No gapi client found; call initGapiClient with your API'
-                    + 'key to make a direct API call. Using server instead');
+      console.error(
+          'No gapi client found; call initGapiClient with your API' +
+          'key to make a direct API call. Using server instead');
       makeDirectApiCall = false;
     }
     if (makeDirectApiCall) {
-      const attributeScores: AttributeScores  = {};
+      const attributeScores: AttributeScores = {};
 
       const attribute = data.modelName || TOXICITY_ATTRIBUTE;
       attributeScores[attribute] = {
-        summaryScore: { value: data.commentMarkedAsToxic ? 1 : 0 }
+        summaryScore: {value: data.commentMarkedAsToxic ? 1 : 0}
       };
       const request: SuggestCommentScoreRequest = {
         comment: {text: data.comment},
@@ -122,19 +118,20 @@ export class PerspectiveApiService {
       };
       console.debug('Making a direct API call with gapi');
       return from(
-         this.gapiClient.commentanalyzer.comments.suggestscore(request))
-         .pipe(map(response => response.result));
+                 this.gapiClient.commentanalyzer.comments.suggestscore(request))
+          .pipe(map(response => response.result));
     } else {
       if (serverUrl === undefined) {
         serverUrl = '';
-        console.error('No server url specified for a non-direct API call.'
-                      + ' Defaulting to current hosted address');
+        console.error(
+            'No server url specified for a non-direct API call.' +
+            ' Defaulting to current hosted address');
       }
       const headers = new HttpHeaders();
       headers.append('Content-Type', 'application/json');
 
       return this.httpClient.post(
-        serverUrl + '/suggest_score', data, {headers});
+          serverUrl + '/suggest_score', data, {headers});
     }
   }
 }
