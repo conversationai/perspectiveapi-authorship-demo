fec8b6da6700b9b4d99c61f5bda79f21cb081445
Rachel Rosen <rachelrosen@google.com>
2020-02-10 10:23:24 -0500

    Removes commented out imports.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -39,16 +39,8 @@
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-
-/** Evergreen browsers require these. **/
-// import 'core-js/es/reflect';
-// import 'core-js/es/reflect';
-
-
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
